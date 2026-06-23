90b722d1b4428af013e39c330e75eb9932a1c3a5
AikyDJ <96826093+AikyDJ@users.noreply.github.com>
2026-06-23 10:51:11 +0300

    controller + service + dependencies

--- src/main/java/com/voly_saina/VolySainaApplication.java
+++ src/main/java/com/voly_saina/VolySainaApplication.java
@@ -1,13 +1,27 @@
 package com.voly_saina;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.voly_saina.properties.StorageProperties;
+import com.voly_saina.service.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)	
 public class VolySainaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VolySainaApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return args -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }

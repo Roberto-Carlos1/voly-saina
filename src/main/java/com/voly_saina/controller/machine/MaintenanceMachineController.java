4bd85634b3ea46f3d24a55049a96687c565b039f
AikyDJ <96826093+AikyDJ@users.noreply.github.com>
2026-06-27 00:01:04 +0300

    Crud maintenance macnine + sercive maintenance

--- src/main/java/com/voly_saina/controller/machine/MaintenanceMachineController.java
+++ src/main/java/com/voly_saina/controller/machine/MaintenanceMachineController.java
@@ -1,25 +1,35 @@
 package com.voly_saina.controller.machine;
 
 import com.voly_saina.entity.MaintenanceMachine;
+import com.voly_saina.repository.MachineRepository;
 import com.voly_saina.service.MaintenanceMachineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/maintenances-machine")
 public class MaintenanceMachineController {
 
+    private final MachineRepository machineRepository;
     @Autowired
     private MaintenanceMachineService maintenanceMachineService;
 
+    MaintenanceMachineController(MachineRepository machineRepository) {
+        this.machineRepository = machineRepository;
+    }
+
     // GET /api/maintenances-machine
     @GetMapping
-    public ResponseEntity<List<MaintenanceMachine>> getAll() {
-        return ResponseEntity.ok(maintenanceMachineService.findAll());
+    public String getAll(Model model) {
+        model.addAttribute("maintenances", maintenanceMachineService.findAll());
+        return "/machines/maintenance/list";
     }
 
     // GET /api/maintenances-machine/{id}
@@ -32,14 +42,28 @@ public class MaintenanceMachineController {
 
     // POST /api/maintenances-machine
     @PostMapping
-    public ResponseEntity<MaintenanceMachine> create(@RequestBody MaintenanceMachine maintenanceMachine) {
-        MaintenanceMachine saved = maintenanceMachineService.save(maintenanceMachine);
+    public ResponseEntity<MaintenanceMachine> create(@RequestParam("idMachine") Long idMachine,
+            @RequestParam("dateCreation") LocalDate dateCreation, @RequestParam("travaux") String travaux,
+            @RequestParam("cout") double cout) {
+
+        MaintenanceMachine saved = maintenanceMachineService.createMaintenanceMachine(idMachine, travaux, cout,
+                dateCreation);
         return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
+
+    // POST /api/maintenances-machine/validate
+    @PostMapping("/validate")
+    public ResponseEntity<MaintenanceMachine> validate(@RequestParam("idMaintenance") Long idMaintenance,
+            @RequestParam("idMachine") Long idMachine, @RequestParam("dateRetourReelle") LocalDate dateRetourReelle) {
+        MaintenanceMachine validated = maintenanceMachineService.validateMaintenanceMachine(idMaintenance, idMachine,
+                dateRetourReelle);
+        return ResponseEntity.ok(validated);
     }
 
     // PUT /api/maintenances-machine/{id}
     @PutMapping("/{id}")
-    public ResponseEntity<MaintenanceMachine> update(@PathVariable Long id, @RequestBody MaintenanceMachine maintenanceMachine) {
+    public ResponseEntity<MaintenanceMachine> update(@PathVariable Long id,
+            @RequestBody MaintenanceMachine maintenanceMachine) {
         if (!maintenanceMachineService.existsById(id)) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
--- src/main/java/com/voly_saina/service/MaintenanceMachineService.java
+++ src/main/java/com/voly_saina/service/MaintenanceMachineService.java
@@ -2,18 +2,75 @@ package com.voly_saina.service;
 
 import com.voly_saina.entity.Machine;
 import com.voly_saina.entity.MaintenanceMachine;
+import com.voly_saina.entity.StatutMachine;
+import com.voly_saina.repository.EtatMachineRepository;
+import com.voly_saina.repository.MachineRepository;
 import com.voly_saina.repository.MaintenanceMachineRepository;
+import com.voly_saina.repository.StatutMachineRepository;
+import com.voly_saina.repository.StatutMaintenanceRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class MaintenanceMachineService {
 
+    private final StatutMachineService statutMachineService;
     @Autowired
     private MaintenanceMachineRepository maintenanceMachineRepository;
+    private EtatMachineRepository etatMachineRepository;
+    private StatutMachineRepository statutMachineRepository;
+    private MachineRepository machineRepository;
+    private StatutMaintenanceRepository statutMaintenanceRepository;
+
+    MaintenanceMachineService(StatutMachineService statutMachineService) {
+        this.statutMachineService = statutMachineService;
+    }
+
+    // create a maintenace machine
+    public MaintenanceMachine createMaintenanceMachine(Long idMachine, String travaux, double cout,
+            LocalDate dateCreation) {
+        MaintenanceMachine maintenanceMachine = new MaintenanceMachine();
+        maintenanceMachine.setDateDebut(dateCreation);
+        maintenanceMachine.setDateRetourPrevue(dateCreation.plusDays(7));
+        maintenanceMachine.setTravaux(travaux);
+        maintenanceMachine.setCout(BigDecimal.valueOf(cout));
+        maintenanceMachine.setMachine(machineRepository.findById(idMachine).orElse(null));
+        maintenanceMachine.setStatutMaintenance(statutMaintenanceRepository.findByCode("prevue").orElse(null));
+
+        StatutMachine status = new StatutMachine();
+        status.setMachine(machineRepository.findById(idMachine).orElse(null));
+        status.setEtatMachine(etatMachineRepository.findByCode("maintenance").orElse(null));
+        status.setDateCreation(dateCreation);
+        statutMachineRepository.save(status);
+        
+        return maintenanceMachineRepository.save(maintenanceMachine);
+    }
+
+    // validate maintenance machine
+    public MaintenanceMachine validateMaintenanceMachine(Long idMaintenance, Long idMachine,
+            LocalDate dateRetourReelle) {
+        Optional<MaintenanceMachine> optionalMaintenanceMachine = maintenanceMachineRepository.findById(idMaintenance);
+        if (optionalMaintenanceMachine.isPresent()) {
+            MaintenanceMachine maintenanceMachine = optionalMaintenanceMachine.get();
+            maintenanceMachine.setDateRetourReelle(dateRetourReelle);
+            maintenanceMachine.setStatutMaintenance(statutMaintenanceRepository.findByCode("terminee").orElse(null));
+
+            StatutMachine status = new StatutMachine();
+            status.setMachine(machineRepository.findById(idMachine).orElse(null));
+            status.setEtatMachine(etatMachineRepository.findByCode("maintenance").orElse(null));
+            status.setDateCreation(dateRetourReelle);
+            statutMachineRepository.save(status);
+            return maintenanceMachineRepository.save(maintenanceMachine);
+        } else {
+            throw new RuntimeException("Maintenance machine not found with id: " + idMaintenance);
+        }
+    }
 
     public List<MaintenanceMachine> findAll() {
         return maintenanceMachineRepository.findAll();
@@ -22,9 +79,11 @@ public class MaintenanceMachineService {
     public Optional<MaintenanceMachine> findById(Long id) {
         return maintenanceMachineRepository.findById(id);
     }
+
     public List<MaintenanceMachine> findByMachine(Machine machineId) {
         return maintenanceMachineRepository.findByMachine(machineId);
     }
+
     public MaintenanceMachine save(MaintenanceMachine maintenanceMachine) {
         return maintenanceMachineRepository.save(maintenanceMachine);
     }

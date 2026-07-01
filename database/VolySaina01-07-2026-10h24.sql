2feba04a06622273279f68211feb008f99fb1eb3
Tendrisoa Nantenaina <ralaiarimangatendry@gmail.com>
2026-07-01 10:44:48 +0300

    ajout table panier

---
+++ database/VolySaina01-07-2026-10h24.sql
@@ -0,0 +1,9 @@
+-- Active: 1773080411410@@127.0.0.1@5432@voly_saina@voly_saina
+create table panier(
+    id_panier SERIAL PRIMARY KEY,
+    id_client INT NOT NULL REFERENCES utilisateur(id_utilisateur) ON DELETE CASCADE,
+    id_commande INT REFERENCES commande(id_commande) ON DELETE CASCADE,
+    id_reservation_machine INT REFERENCES reservation_machine(id_reservation) ON DELETE CASCADE,
+    date_arret TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+

731a44475beb7f6b1552b31f941df2fff4c44bbe
AikyDJ <96826093+AikyDJ@users.noreply.github.com>
2026-07-14 14:53:14 +0300

    Reapply "route api ligne commade & commande & facture"
    This reverts commit 29f349ea00e528dc588445ec4750403b5ef8b928.

--- src/main/java/com/voly_saina/service/CommandeService.java
+++ src/main/java/com/voly_saina/service/CommandeService.java
@@ -1,21 +1,49 @@
 package com.voly_saina.service;
 
+import com.voly_saina.dto.CommandeClientDTO;
+import com.voly_saina.dto.LigneCommandeDTO;
 import com.voly_saina.entity.Commande;
+import com.voly_saina.entity.LigneCommande;
 import com.voly_saina.repository.CommandeRepository;
+import com.voly_saina.repository.LigneCommandeRepository;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
-import com.voly_saina.entity.Commande;
-import com.voly_saina.repository.CommandeRepository;
 
 @Service
 public class CommandeService {
 
     @Autowired
     private CommandeRepository commandeRepository;
+
+    @Autowired
+    private LigneCommandeRepository LigneCommandeRepository;
+
+    public CommandeClientDTO convertToCommandeClientDTO(Commande commande) {
+        CommandeClientDTO dto = new CommandeClientDTO();
+        dto.setIdCommande(commande.getIdCommande());
+        dto.setNumeroCommande(commande.getIdCommande().toString());
+        dto.setStatutCommande(commande.getStatutCommande().getCode());
+        dto.setDateCommande(commande.getDateCommande().toString());
+        dto.setDateLivraison(commande.getDateCommande() != null ? commande.getDateCommande().toString() : null);
+        dto.setModePaiement(commande.getModePaiement().getLibelle());
+        dto.setAdresseLivraison(commande.getAdresseLivraison());
+        return dto;
+    }
+
+    public LigneCommandeDTO convertToLigneCommandeDTO(LigneCommande lignesCommande) {
+        LigneCommandeDTO dto = new LigneCommandeDTO();
+        dto.setIdLigneCommande(lignesCommande.getIdLigne());
+        dto.setIdCommande(lignesCommande.getCommande().getIdCommande());
+        dto.setIdProduit(lignesCommande.getProduit().getIdProduit());
+        dto.setNomProduit(lignesCommande.getProduit().getNom());
+        dto.setQuantite(lignesCommande.getQuantite().intValue());
+        dto.setPrixUnitaire(lignesCommande.getPrixUnitaire());
+        return dto;
+    }   
 
     public List<Commande> findAll() {
         return commandeRepository.findAll();
@@ -41,4 +69,17 @@ public class CommandeService {
         return commandeRepository.findByClientIdUtilisateur(id);
     }
 
+    // get client commandes by idClient
+    public List<CommandeClientDTO> getCommandesByClient(Long idClient) {
+        List<Commande> commandes = commandeRepository.findByClientIdUtilisateur(idClient);
+        return commandes.stream().map(this::convertToCommandeClientDTO).toList();
+    }
+    // get client comandes details by idClient
+public List<LigneCommandeDTO> getLignesCommandesByClient(Long idClient) {
+        List<Commande> commandes = commandeRepository.findByClientIdUtilisateur(idClient);
+        return commandes.stream()
+                .flatMap(commande -> LigneCommandeRepository.findByCommandeIdCommande(commande.getIdCommande()).stream())
+                .map(this::convertToLigneCommandeDTO)
+                .toList();
+    }
 }

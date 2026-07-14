0189e8e21bc9154ce97f66d7082da43ae65fe9df
AikyDJ <96826093+AikyDJ@users.noreply.github.com>
2026-07-14 18:58:07 +0300

    Squashed commit of the following:
    commit 55319ca27f96e813a3ce29511339e17952cc91c8
    Author: AikyDJ <96826093+AikyDJ@users.noreply.github.com>
    Date:   Tue Jul 14 18:54:44 2026 +0300
    
        revert css et bug faturation rectifer

--- src/main/java/com/voly_saina/service/StatutFactureService.java
+++ src/main/java/com/voly_saina/service/StatutFactureService.java
@@ -33,4 +33,8 @@ public class StatutFactureService {
     public void deleteById(Long id) {
         statutFactureRepository.deleteById(id);
     }
+
+    public Optional<StatutFacture> findByCode(String code) {
+        return statutFactureRepository.findByCode(code);
+    }
 }
